a37318b7aa8ddd65a673243811b7019bfade5ade
LukaB0 <github@buncic.io>
2025-11-03 11:35:53 -0500

    added character counter function

--- main.py
+++ main.py
@@ -1,8 +1,10 @@
-from stats import get_book_word_count
+from stats import get_book_word_count, character_count
 
 def main():
     word_count = get_book_word_count()
+    char_count = character_count()
     print(f"Found {word_count} total words")
+    print(char_count)
 
 if __name__ == "__main__":
     main()
--- stats.py
+++ stats.py
@@ -6,3 +6,14 @@ def get_book_word_count ():
     contents = get_book_text("books/frankenstein.txt")
     words = contents.split()
     return len(words)
+
+def character_count ():
+    dict_chars = {}
+    characters = get_book_text("books/frankenstein.txt")
+    lower_chars = characters.lower()
+    for c in lower_chars:
+        if c in dict_chars:
+            dict_chars[c] = dict_chars[c] +1
+        else:
+            dict_chars[c] = 1
+    return dict_chars
