66aec12eec3161a025e480439eb844ea8b6edab6
LukaB0 <github@buncic.io>
2025-11-03 11:00:29 -0500

    word count function added

--- main.py
+++ main.py
@@ -2,9 +2,14 @@ def get_book_text (path):
     with open(path) as f:
         return f.read()
 
-def main():
+def get_book_word_count ():
     contents = get_book_text("books/frankenstein.txt")
-    print(contents)
+    words = contents.split()
+    return len(words)
+
+def main():
+    word_count = get_book_word_count()
+    print(f"Found {word_count} total words")
 
 if __name__ == "__main__":
     main()
